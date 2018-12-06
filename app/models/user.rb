6140f1c806d27c6be4c489b064e2fda0c6dfcb05
dereksmith83 <mdsmith1983@gmail.com>
2018-12-05 20:17:05 -0600

    added orders association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+
+  # has_many :orders
+  # has_many :tickets, through: :orders
+  # has_many :events #event_producer events
+  # has_many :tickets, through: :events #event_producer sales
 end
