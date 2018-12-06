6ed26043c06712718a95b0d0f1ea23aa3c437efd
dereksmith83 <mdsmith1983@gmail.com>
2018-12-05 19:10:15 -0600

    migrated User model data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_234746) do
+ActiveRecord::Schema.define(version: 2018_12_06_010851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2018_12_05_234746) do
     t.string "name"
     t.integer "event_id"
     t.decimal "price", precision: 9, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
